bb64b0184af2bcb29c49d678ebc02886db9e6507
SAMUEL HAILE <samishken@gmail.com>
2018-03-01 16:13:55 -0500

    finish create and show joke

--- controllers/joke.js
+++ controllers/joke.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const Joke = require('../models/Joke')
 
 // Define our routes
 // get all jokes: '/'
 router.post('/', (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
 })
 // create new joke
+router.post('/', (req, res) => {
+  Joke.create(req.boy).then(joke => {
+    res.redirect(`/jokes/${joke.id}`)
+  })
+})
 // get new view for a new joke
 router.get('/new')
- 
+
 router.get('/jokes/new', (req, res) => {
   res.send('hello from the other side')
 })
 
-//get a joke by it's id: '/:id'
+// get a joke by it's id: '/:id'
+router.get('/:id', (req, res) => {
+  Joke.findOne({_id: req.params.id }).then(joke => {
+    res.render('joke/show', joke)
+  })
+})
+// update a joke by it's id: '/:id'
 
-//update a joke by it's id: '/:id'
-
-//delete a joke by it's id: '/:id'
+// delete a joke by it's id: '/:id'
 // export our router
 module.exports = router
